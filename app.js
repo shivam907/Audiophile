022d3a5e7db7fca027e027828589b7467574ccc4
shivam907 <shivamkaushal907@gmail.com>
2022-12-19 23:25:39 +0530

    Setup Backend

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+require("dotenv").config();
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 
 app.listen(process.env.PORT || 3000);

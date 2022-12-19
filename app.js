338edae99b7037bc7eff52459c8c235f3a5e381c
shivam907 <shivamkaushal907@gmail.com>
2022-12-19 23:18:08 +0530

    ignored some files

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 3000);

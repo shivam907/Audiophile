73fecc01efee9812f5ab5362005f013996e77c1d
shivam907 <shivamkaushal907@gmail.com>
2024-02-10 01:42:01 +0530

    added form error handling

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import classes from "./Checkout.module.css";
 import Payment from "../Modals/Payment";
 import data from "../../Data/products.json"
+import Input from "../Input/Input";
 import { useSelector } from "react-redux";
 const Checkout = () => {
   const cart = useSelector(state=>state.cart)
   const [cartItem, setCartItem] = React.useState()
   const [pay, setPay] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+  const name = React.useRef();
+  const email = React.useRef();
+  const number = React.useRef();
+  const address = React.useRef();
+  const zip = React.useRef();
+  const city = React.useRef();
+  const state = React.useRef();
+  const country = React.useRef();
+  const money = React.useRef();
+  const pin = React.useRef();
   const sett = () => {
-    setPay(true);
+    console.log(name.current.value.length);
+    // setPay(true);
   };
   const resett = () => {
     setPay(false);
@@ -49,54 +62,62 @@ const Checkout = () => {
               <div className={classes["bill"]}>
                 <h1>billing details</h1>
                 <div className={classes["billing"]}>
-                  <div className={classes["bill1"]}>
-                    <p>Name</p>
-
-                    <input type="text" placeholder="Shivam Kaushal" />
-                  </div>
-                  <div className={classes["bill1"]}>
-                    <p>Email Address</p>
-
-                    <input type="email" placeholder="shivam@syncu.me" />
-                  </div>
-                  <div className={classes["bill1"]}>
-                    <p>Phone Number</p>
-
-                    <input type="tel" placeholder="+91 8968969929" />
-                  </div>
+                  <Input
+                    label="Name"
+                    reff={name}
+                    type="text"
+                    placeholder="Shivam kaushal"
+                  />
+                  <Input
+                    label="Email Address"
+                    type="email"
+                    placeholder="shivamkaushal907@gmail.com"
+                    reff={email}
+                  />
+                  <Input
+                    label="Phone Number"
+                    type="tel"
+                    placeholder="+91 9999999999"
+                    reff={number}
+                  />
                 </div>
               </div>
               <div className={classes["bill"]}>
                 <h1>shipping info</h1>
                 <div className={classes["billing2"]}>
-                  <div className={classes["bill1"]}>
-                    <p>Your Address</p>
-
-                    <input type="text" placeholder="655 Vakillan Bazar" />
+                  <Input
+                    label="Your Adress"
+                    type="text"
+                    placeholder="111 Model Town"
+                    reff={address}
+                  />
+                  <div className={classes["billi"]}>
+                    <Input
+                      label="Zip Code"
+                      type="number"
+                      placeholder="146001"
+                      reff={zip}
+                    />
+                    <Input
+                      label="City"
+                      type="text"
+                      placeholder="Hoshiarpur"
+                      reff={city}
+                    />
                   </div>
                   <div className={classes["billi"]}>
-                    <div className={classes["bill1"]}>
-                      <p>Zip Code</p>
-
-                      <input type="number" placeholder="146001" />
-                    </div>
-                    <div className={classes["bill1"]}>
-                      <p>City</p>
-
-                      <input type="text" placeholder="Hoshiarpur" />
-                    </div>
-                  </div>
-                  <div className={classes["billi"]}>
-                    <div className={classes["bill1"]}>
-                      <p>State</p>
-
-                      <input type="text" placeholder="Punjab" />
-                    </div>
-                    <div className={classes["bill1"]}>
-                      <p>Country</p>
-
-                      <input type="text" placeholder="India" />
-                    </div>
+                    <Input
+                      label="State"
+                      type="text"
+                      placeholder="Punjab"
+                      reff={state}
+                    />
+                    <Input
+                      label="Country"
+                      type="text"
+                      placeholder="India"
+                      reff={country}
+                    />
                   </div>
                 </div>
               </div>
@@ -109,25 +130,47 @@ const Checkout = () => {
                     </div>
                     <form className={classes["billi2"]}>
                       <div className={classes["bool"]}>
-                        <input name="A" type="radio" placeholder="e-Money" />
+                        <input
+                          checked
+                          onClick={() => setSelected(0)}
+                          name="A"
+                          type="radio"
+                          placeholder="e-Money"
+                        />
                         <h1>e-Money</h1>
                       </div>
                       <div className={classes["bool"]}>
-                        <input name="A" type="radio" placeholder="Cash on Delivery" />
+                        <input
+                          onClick={() => setSelected(1)}
+                          name="A"
+                          type="radio"
+                          placeholder="Cash on Delivery"
+                        />
                         <h1>Cash on Delivery</h1>
                       </div>
                     </form>
                   </div>
-                  <div className={classes["billi"]}>
-                    <div className={classes["bill1"]}>
-                      <p>e-Money Number</p>
-                      <input type="number" placeholder="12345678" />
+                  {selected == 0 ? (
+                    <div className={classes["billi"]}>
+
+                      <Input label="e-Money Number" type="number" placeholder="12345678" reff={money} />
+                      <Input label="e-Money Pin" type="number" placeholder="1234" reff={pin} />
+
                     </div>
-                    <div className={classes["bill1"]}>
-                      <p>e-Money Pin</p>
-                      <input type="number" placeholder="1234" />
+                  ) : (
+                    <div className={classes.cod}>
+                      <img
+                        src="../../Images/shared/desktop/payment.svg"
+                        alt=""
+                      />
+                      <p>
+                        The ‘Cash on Delivery’ option enables you to pay in cash
+                        when our delivery courier arrives at your residence.
+                        Just make sure your address is correct so that your
+                        order will not be cancelled.
+                      </p>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -137,9 +180,7 @@ const Checkout = () => {
                   <div className={classes["cartrow1"]}>
                     <h1>SUMMARY</h1>
                   </div>
-                  <div className={classes.cartRows}>
-                  {cartItem}
-                  </div>
+                  <div className={classes.cartRows}>{cartItem}</div>
                   <div className={classes["cr2"]}>
                     <div className={classes["cr3"]}>
                       <p>totall</p>
@@ -153,7 +194,7 @@ const Checkout = () => {
                   </div>
                   <div className={classes["cr3"]}>
                     <p>grand totall</p>
-                    <h1 className={classes["grand"]}>$ {cart.price+50}</h1>
+                    <h1 className={classes["grand"]}>$ {cart.price + 50}</h1>
                   </div>
                   <div className={classes["cartbtn"]} onClick={sett}>
                     CONTINUE & PAY
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "../Checkout/Checkout.module.css";
+const Input = (props) => {
+  return (
+    <div className={`${classes["bill1"]} ${classes.err}`}>
+      <div className={classes.label}>
+        <p>{props.label}</p>
+        <p className={classes.error}>Wrong Format</p>
+      </div>
+      <input
+        ref={props.reff}
+        type={`${props.type}`}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;

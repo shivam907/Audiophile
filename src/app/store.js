d49377feb34d82893590e41a4224037e8eacdb92
shivam907 <shivamkaushal907@gmail.com>
2024-02-09 13:23:22 +0530

    presisted the redux store state in localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
-import cartSlice from '../features/cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "../features/cartSlice";
+const localStorageMiddleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem("applicationState", JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  if (localStorage.getItem("applicationState") !== null) {
+    return JSON.parse(localStorage.getItem("applicationState"));
+  }
+};
+
 export const store = configureStore({
-    reducer: cartSlice
-})
+  reducer: cartSlice,
+    preloadedState: reHydrateStore(),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+});
